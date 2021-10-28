aa79faa5068fbc842e9852a24287c80cb74b8e1a
Megan Duclos <megan-duclos@pluralsight.com>
2021-10-28 13:37:14 -0600

    Add review card styles

--- src/components/reviewsList.tsx
+++ src/components/reviewsList.tsx
@@ -1,16 +1,32 @@
 import StarRatings from "react-star-ratings";
-
+import "./reviewsList.css";
+import hamlet from "../assets/hamlet.png";
+import juliet from "../assets/juliet.png";
+import ladymacbeth from "../assets/ladymacbeth.png";
+import romeo from "../assets/romeo.png";
+import witch from "../assets/witch.png";
 import { Review } from "../interfaces";
+
+const avatars = [hamlet, juliet, ladymacbeth, romeo, witch];
 
 export const ReviewsList = (props: { reviews: Review[] }) => {
   return (
-    <div>
+    <div className="reviewList">
       {props.reviews.map((review) => (
-        <div key={review.id} className="reviewList">
-          <StarRatings rating={review.rating} />
-          <p>name: {review.author}</p>
-          <p>body: {review.body}</p>
-          <p>date: {review.publishDate.toDateString()}</p>
+        <div key={review.id} className="reviewCard">
+          <img
+            src={avatars[Math.floor(Math.random() * avatars.length)]}
+            alt="avatar-image"
+          />
+          <div className="rating">
+            <p>{review.author}</p>
+            <StarRatings
+              rating={review.rating}
+              starDimension="15px"
+              starSpacing="1px"
+              starRatedColor="#FDCC0D"
+            />
+          </div>
         </div>
       ))}
     </div>
